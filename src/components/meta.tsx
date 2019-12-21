f6c775960e67424d95661db2bb6801b4b1437193
ichik <ichikumer@gmail.com>
2019-12-21 12:54:38 +0100

    More meta tinkering

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -2,12 +2,50 @@ import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import Helmet from "react-helmet"
 
-const Meta = () => {
+type Props = {} & typeof defaultProps
+
+const defaultProps = {
+  title: "",
+  desc: "",
+  banner: "",
+  pathname: "",
+  node: {
+    modifiedTime: "",
+    birthTime: "",
+  },
+}
+
+const Meta = ({ title, desc, banner, pathname, node }: Props) => {
   const { site } = useStaticQuery(query)
-  return <Helmet title={site.siteMetadata.defaultTitle}></Helmet>
+  const {
+    buildTime,
+    siteMetadata: {
+      siteUrl,
+      defaultTitle,
+      defaultDescription,
+      siteLanguage,
+      ogLanguage,
+      author,
+    },
+  } = site
+  const seo = {
+    title: title || defaultTitle,
+    description: desc || defaultDescription,
+    // image: `${siteUrl}${banner || defaultBanner}`,
+    url: `${siteUrl}${pathname || ""}`,
+  }
+  return (
+    <Helmet title={seo.title}>
+      <html lang={siteLanguage} />
+      <meta name="description" content={seo.description} />
+      <meta name="author" content={author} />
+    </Helmet>
+  )
 }
 
 export default Meta
+
+Meta.defaultProps = defaultProps
 
 const query = graphql`
   query Meta {
