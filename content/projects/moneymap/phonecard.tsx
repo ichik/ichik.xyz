43dc1c584bc99d2a73dbc114ab5932fbac2401bd
ichik <ichikumer@gmail.com>
2020-01-03 15:38:28 +0100

    Smoother movement

--- content/projects/moneymap/phonecard.tsx
+++ content/projects/moneymap/phonecard.tsx
@@ -11,12 +11,12 @@ const Card = styled.div`
   width: 704px;
   height: 248px;
   margin: 1rem 0;
-  transition: all 0.1s ease-in;
+  transition: transform 0.3s ease-in-out, box-shadow 0.2s ease-in;
 
   &:hover {
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.4);
     cursor: pointer;
-    transform: translateY(-0.5rem);
+    transform: translate3D(0, -0.5rem, 0);
   }
 `
 
