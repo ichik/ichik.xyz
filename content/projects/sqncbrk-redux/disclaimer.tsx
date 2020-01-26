feae22d299a2c34175c3cb91a1fb25ba3e620041
ichik <ichikumer@gmail.com>
2020-01-26 14:16:24 +0100

    Visual impovements

--- content/projects/sqncbrk-redux/disclaimer.tsx
+++ content/projects/sqncbrk-redux/disclaimer.tsx
@@ -1,9 +1,10 @@
 import styled from "styled-components"
 
 const Disclaimer = styled.div`
-  background-color: #362800;
+  background-color: hsla(32, 100%, 50%, 10%);
   max-width: 44rem;
   padding: 2rem;
+  margin: 1rem 0;
   border-radius: 0.5rem;
 `
 
