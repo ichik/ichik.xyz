bd1efcf172f975a93f659f93724966c479bbe4db
ichik <ichikumer@gmail.com>
2020-01-26 19:09:40 +0100

    SqncBrk stub

---
+++ content/projects/sqncbrk/disclaimer.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+const Disclaimer = styled.div`
+  background-color: hsla(32, 100%, 50%, 10%);
+  max-width: 44rem;
+  padding: 2rem;
+  margin: 1rem 0;
+  border-radius: 0.5rem;
+`
+
+export default Disclaimer
---
+++ content/projects/sqncbrk/newlogo.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import styled from "styled-components"
+
+const mp4 = require("./newlogo.mp4")
+const poster = require("./newlogo.jpg")
+
+const Player = styled.div`
+  > video {
+    max-width: 100%;
+  }
+`
+
+const Newlogo = () => (
+  <Player>
+    <video preload="auto" autoPlay loop muted playsinline poster={poster}>
+      <source src={mp4} type="video/mp4" />
+    </video>
+  </Player>
+)
+
+export default Newlogo
