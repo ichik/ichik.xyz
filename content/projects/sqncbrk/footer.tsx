f913cdb0812b006c5976d237ba0fd7fc90e9e305
ichik <ichikumer@gmail.com>
2020-02-02 15:10:05 +0100

    SqncBrk 100%

---
+++ content/projects/sqncbrk/footer.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import styled from "styled-components"
+
+const mp4 = require("./footer.mp4")
+const poster = require("./footer.jpg")
+
+const Player = styled.div`
+  > video {
+    max-width: 100%;
+  }
+`
+
+const Footer = () => (
+  <Player>
+    <video preload="auto" autoPlay loop muted playsinline poster={poster}>
+      <source src={mp4} type="video/mp4" />
+    </video>
+  </Player>
+)
+
+export default Footer
